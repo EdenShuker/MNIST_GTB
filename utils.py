c541252ac5525f634cf4d6fb6b52df414d3359c8
Tamir Moshiashvili <norblet82@gmail.com>
2018-01-08 12:46:19 +0200

    # cv

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import random
 from time import time
 import mnist
+import numpy as np
 import xgboost as xgb
+from sklearn.metrics import accuracy_score
 
 
 def main1():
@@ -27,11 +29,51 @@ def main2():
     print res
 
 
+def main3():
+    # load train data
+    mndata = mnist.MNIST('data')
+    train, train_labels = mndata.load_training()
+    train, train_labels = np.array(train), np.array(train_labels)  # as numpy
+    print 'loaded data'
+
+    # train a model
+    model = xgb.XGBClassifier(n_estimators=1)
+    model.fit(train, train_labels)
+    print model  # just for checking on it
+
+    # predict on test
+    test, test_labels = mndata.load_testing()
+    test, test_labels = np.array(test), np.array(test_labels)
+    preds = model.predict(test)
+    predictions = [round(val) for val in preds]
+
+    # accuracy
+    acc = accuracy_score(test_labels, predictions)
+    print 'accuracy %0.2f%%' % (acc * 100.0)
+
+
+def main4():
+    def data_as_dmat(data_and_labels):
+        data, labels = data_and_labels
+        data, labels = np.array(data), np.array(labels)
+        return xgb.DMatrix(data, labels)
+
+    mndata = mnist.MNIST('data')
+    train_dmat = data_as_dmat(mndata.load_training())
+
+    params = {'eta': 0.1, 'seed': 0, 'max_depth': 3, 'num_class': 10}
+    res = xgb.cv(params, train_dmat, num_boost_round=1, nfold=3, metrics=['merror'])
+    # print res
+    print 'accuracy:', 1 - res['test-merror-mean'][0]
+
+
 if __name__ == '__main__':
     t = time()
     print 'start'
 
     # main1()
-    main2()
+    # main2()
+    # main3()
+    main4()
 
     print 'time for running all:', time() - t
